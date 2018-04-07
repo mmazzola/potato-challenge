6766af31b00d259b232d91e35c41a35243e685e5
Matteo Mazzola <matteo.mazzola90@gmail.com>
2018-04-08 00:21:12 +0100

    Search bar implemented

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router,Route,Link, Switch} from 'react-router-dom'
+import {BrowserRouter as Router,Route,Link, Switch} from 'react-router-dom';
 import {InfiniteLoader, List,WindowScroller} from 'react-virtualized';
 import MediaQuery from 'react-responsive';
+import Autosuggest from 'react-autosuggest';
 import './index.css';
 
 const IS_LOADING = 1
@@ -16,18 +17,52 @@ const IMG_PLACEHOLDER_URL = "http://via.placeholder.com/200x200?text=?"
 
 require('es6-promise').polyfill();
 var fetchJsonp=require('fetch-jsonp');
-class Header extends React.Component {
-  render() {
+
+class MainView extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchList : [],
+    }
+    this.updateSearchList = this.updateSearchList.bind(this)
+    this.onValueSelected = this.onValueSelected.bind(this)
+    this._List = React.createRef();
+
+  }
+
+  updateSearchList(newList)
+  {
+    var result = [];
+    newList.map((i) =>{return i.tags}).forEach(
+      (val,index)=>
+      {
+        var parts = val.split(" ");
+        parts.forEach((val, index)=>{
+          if(!result.includes(val))
+          {
+            result.push(val);
+          } 
+        })
+      }
+    )
+    this.setState({searchList : result});
+  }
+
+  onValueSelected(value){
+    this._List.current.filterList(value);
+  }
+
+  render(){
     return (
-      <header>
-        <h1>Matteo Mazzola - Potato Challenge</h1>
-      </header>
-    );
+      <div>
+        <SearchBox suggestions={this.state.searchList} onValueSelected={this.onValueSelected}/>
+        <FlickrList onListFetched={this.updateSearchList} ref={this._List}/>
+      </div>
+    )
   }
 }
 
 class FlickrList extends React.Component {
-
   constructor(props){
     super(props);
     this.state = {
@@ -35,6 +70,7 @@ class FlickrList extends React.Component {
         loadedRowsMap: {},
         loadingRowCount: 0,
         list : [],
+        backupList : [],
         screen:
         {
           width : 0,
@@ -48,6 +84,8 @@ class FlickrList extends React.Component {
       this.loadMoreRows = this.loadMoreRows.bind(this)
       this.getRowHeight = this.getRowHeight.bind(this)
       this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+      this.filterList = this.filterList.bind(this);
+      this.noRowsRenderer = this.noRowsRenderer.bind(this);
     }
     componentWillMount(){
       this.loadMoreRows(0,5);
@@ -76,6 +114,29 @@ class FlickrList extends React.Component {
       }
     }
 
+  //Handle search value on tags  
+  filterList(value){
+    if(value)
+    {
+      this.setState(
+        {
+          list : this.state.backupList.filter( (p)=>{
+            return p.tags.split(" ").filter(
+              (t) =>{
+                return t.startsWith(value);
+              }
+            ).length > 0;
+          })
+      })
+    }
+    else{
+      this.setState(
+        {
+          list : this.state.backupList
+        })
+    }
+  }
+
   //Handles different screen sizes for the row height of the list
   getRowHeight(){
     return this.state.screen.width * this.state.screen.rowScaleDown; 
@@ -83,6 +144,20 @@ class FlickrList extends React.Component {
   
   // Manage loading state of rows
   isRowLoaded({index}){ return !!this.state.loadedRowsMap[index];}
+
+  //Render an empty list
+  noRowsRenderer(){
+    let text = this.state.backupList.length ? "Sorry, there are no posts with this tag :("
+    : "Just a second...";
+    return(
+    <div className="post" style={{justifyContent:"center"}}>
+      <h1 style={{maxWidth:this.state.screen.width * this.state.screen.textScaleDown,
+        fontSize: this.state.screen.width * 0.03}}>
+        {text}      
+      </h1>
+    </div>
+  );
+  }
 
   //Render a list item
   rowRenderer({key, index, style, isVisible}){
@@ -142,7 +217,6 @@ class FlickrList extends React.Component {
   loadMoreRows({startIndex, stopIndex}){
     const {loadedRowsMap, loadingRowCount, loadedRowCount } = this.state
     const loadingRows = stopIndex - startIndex + 1
-
     for (var i = startIndex; i <= stopIndex; i++) {
       loadedRowsMap[i] = IS_LOADING
     }
@@ -151,17 +225,22 @@ class FlickrList extends React.Component {
       loadingRowCount: loadingRowCount + loadingRows
     })
     
+    const that = this;
     return fetchJsonp(API_URL,{jsonpCallbackFunction : "jsonFlickrFeed"})
     .then(response=> response.json()
       .then(data => {
         for (var i = startIndex; i <= stopIndex; i++) {
           loadedRowsMap[i] = IS_LOADED
         }
-        this.setState({
+        that.setState({
           loadingRowCount: loadingRowCount - loadingRows,
           loadedRowCount: loadedRowCount + loadingRows,
-          list: data.items
+          list: data.items,
+          backupList : data.items
         })
+        if(that.props.onListFetched){
+          that.props.onListFetched(data.items);
+        }
       })
     );
   }
@@ -187,7 +266,8 @@ class FlickrList extends React.Component {
               rowHeight={this.getRowHeight}
               scrollTop={scrollTop}
               isScrolling = {isScrolling}
-              rowRenderer={this.rowRenderer}/>
+              rowRenderer={this.rowRenderer}
+              noRowsRenderer = {this.noRowsRenderer}/>
             )}
         </WindowScroller>
       )}
@@ -200,7 +280,6 @@ class FlickrList extends React.Component {
 
 class DetailPost extends React.Component
 {
-
   render()
   {
     return (
@@ -211,15 +290,88 @@ class DetailPost extends React.Component
 
 //========================================
 
+class SearchBox extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      trackList : props.trackList,
+      indexList : [],
+      suggestions : props.suggestions,
+      value: ""
+    };
+      this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this)
+      this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this)
+      this.getSuggestionValue = this.getSuggestionValue.bind(this);
+      this.renderSuggestion = this.renderSuggestion.bind(this);
+      this.onChange = this.onChange.bind(this);
+    }
+
+    onSuggestionsFetchRequested({value, reason})
+    {
+     this.setState(
+       {
+         suggestions : this.props.suggestions.filter((val)=>{return val.startsWith(value)})
+      });
+    }
+
+    renderSuggestion(suggestion) {return (<span>{suggestion}</span>)};
+
+    onSuggestionsClearRequested(){this.setState({suggestions : []});}
+
+    getSuggestionValue(suggestion){
+      return suggestion;
+    }
+
+    onChange(event,info){
+      this.setState({value : info.newValue});
+      if(this.props.onValueSelected)
+      {
+        this.props.onValueSelected(info.newValue);
+      }
+    }
+
+    render(){
+      return (
+        <Autosuggest
+        suggestions={this.state.suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={this.getSuggestionValue}
+        renderSuggestion={this.renderSuggestion}
+        inputProps={
+          {
+            placeholder :"Search through picture tags...",
+            value : this.state.value,
+            onChange : this.onChange
+          }
+        }
+        theme={
+          {
+          container : 'search-container',
+          containerOpen : 'search-container-open',
+          input: 'search-input',
+          inputOpen: 'search-input-open',
+          inputFocused:'search-input-focused',
+          suggestionsContainer: 'search-suggestions-container',
+          suggestionsContainerOpen:'search-suggestions-container-open',
+          suggestionsList: 'search-suggestions-list',
+          suggestion: 'search-suggestion',
+          suggestionHighlighted: 'search-suggestion-highlighted'
+          }
+        }
+      />
+      );
+    }
+}
 
 ReactDOM.render(
   <Router>
   <div className="fullscreen">
-    <Header/>
     <div id="container">
       <h1 className="list-header">Flickr Public Feed</h1>
         <Switch>
-          <Route exact path="/" component={FlickrList} />
+          <Route exact path="/" component={MainView} />
           <Route path={DETAIL_URL +"?post=:post"} component={DetailPost} />
         </Switch>
     </div>
